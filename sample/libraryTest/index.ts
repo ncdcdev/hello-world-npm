f1e5d1c83344f4c044cfcfb86e6184f59abb00d7
k-ibaraki <ibaraki@ncdc.co.jp>
2022-12-23 17:36:33 +0900

    サンプルアプリをv0.1.2に更新

--- sample/libraryTest/index.ts
+++ sample/libraryTest/index.ts
@@ -1,9 +1,20 @@
 //import { trim } from "./node_modules/helloworldnpm/dist/hellolib.js";
 import { ElevationLevel } from "elevationlevel";
 
+/*
+--- GL+0.0 = TP+5.5
+
+--- GL-2.4 = TP+3.1
+
+--- GL-5.5 = TP+0.0
+*/
+
+
+
 // GL=TP+5.5
 const config = { GL: { standard: "TP" as const, level: 5.5 } }
 
+// TP+3.1
 const el = new ElevationLevel(
   {
     level: 3.1,
@@ -11,6 +22,7 @@ const el = new ElevationLevel(
   }
 )
 
+// GLに変換 → GL-2.4
 console.log(
   el.to("GL", config) // = Decimal(-2.4)
 )
@@ -28,14 +40,3 @@ console.log(
 //   el.to("TP", config) // = Decimal(6.5)
 // )
 
-/*
-
---- GL+1.0 = TP+6.5
-
---- GL+0.0 = TP+5.5
-
---- GL-2.4 = TP+3.1
-
---- GL-5.5 = TP+0.0
-
-*/
